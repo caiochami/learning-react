d219f7456eedcba80771a44564ebd6db0d944e2b
Caio Chami <caio.chami18@gmail.com>
2022-11-16 19:58:15 -0300

    feat: Finish day three.

--- src/routes/30-days-of-react/day-three/index.jsx
+++ src/routes/30-days-of-react/day-three/index.jsx
@@ -1,83 +1,86 @@
+import { CheckIcon, ClockIcon } from "@heroicons/react/24/outline";
 import React from "react";
+import Button from "../../../components/Button";
+
+const tools = (
+  <div className="bg-gray-200 flex flex-col text-center p-2 gap-4">
+    <p className="font-bold">Front End Technologies</p>
+    <div className="flex flex-row gap-2 justify-between">
+      <img src="/src/assets/html_logo.png" className="w-32 h-32" />
+      <img src="/src/assets/css_logo.png" className="w-32 h-32" />
+      <img src="/src/assets/html_logo.png" className="w-32 h-32" />
+      <img src="/src/assets/react_logo.png" className="w-32 h-32" />
+    </div>
+  </div>
+);
+
+const subscribe = (
+  <div className="w-full bg-cyan-200 rounded-md">
+    <div className="flex flex-col gap-4 text-center p-4 font-mono w-2/3 mx-auto">
+      <p className="text-3xl uppercase">Subscribe</p>
+      <p>Sign up with email address to receive news and updates.</p>
+      <div className="grid grid-cols-3 gap-2">
+        <input
+          type="text"
+          className="p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        />
+        <input
+          type="text"
+          className="p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        />
+        <input
+          type="text"
+          className="p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        />
+      </div>
+      <div>
+        <Button color="warning">Subscribe</Button>
+      </div>
+    </div>
+  </div>
+);
+
+const skills = ["HTML", "CSS", "Sass", "JS", "React", "Mysql", "Gatsby", "Git"];
+
+const userCard = (
+  <div className="shadow-md flex flex-col p-2 gap-6 font-serif">
+    <img
+      src="/src/assets/asabeneh.jpg"
+      className="w-32 h-32 object-contain rounded-full"
+    />
+    <div className="flex gap-2">
+      <div className="uppercase font-semibold">Asabeneth Yetayeh</div>
+      <CheckIcon className="w-5 h-5 text-white bg-teal-400 rounded-full" />
+    </div>
+
+    <p className="font-light">Senior Developer, Finland</p>
+
+    <div className="uppercase">Skills</div>
+
+    <div className="flex flex-wrap gap-2">
+      {skills.map((skill) => (
+        <div
+          className="px-2 py-1 bg-teal-500 text-white font-semibold rounded-md"
+          key={skill}
+        >
+          {skill}
+        </div>
+      ))}
+    </div>
+
+    <div className="flex items-center gap-2 font-thin">
+      <ClockIcon className="w-5 h-5" />
+      Joined on Aug 30, 2020
+    </div>
+  </div>
+);
 
 export default function index() {
-  const welcome = "Welcome to 30 Days Of React Challenge";
-  const title = "Getting Started React";
-  const subtitle = "JavaScript Library";
-  const author = {
-    firstName: "Asabeneh",
-    lastName: "Yetayeh",
-  };
-  const date = "Oct 2, 2020";
-
-  const header = (
-    <div>
-      <div className="bg-blue-300">
-        <h1 className="text-3xl">{welcome}</h1>
-        <h2 className="text-2xl">{title}</h2>
-        <h3>{subtitle}</h3>
-        <p>
-          Instructor: {author.firstName} {author.lastName}
-        </p>
-        <small>Date: {date}</small>
-      </div>
-    </div>
-  );
-
-  const numOne = 3;
-  const numTwo = 2;
-
-  const result = (
-    <p>
-      {numOne} + {numTwo} = {numOne + numTwo}
-    </p>
-  );
-
-  const yearBorn = 1820;
-  const currentYear = 2020;
-  const age = currentYear - yearBorn;
-  const personAge = (
-    <p>
-      {" "}
-      {author.firstName} {author.lastName} is {age} years old
-    </p>
-  );
-
-  const techs = ["HTML", "CSS", "JavaScript"];
-  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
-
-  const main = (
-    <div>
-      <div className="">
-        <p>
-          Prerequisite to get started{" "}
-          <strong>
-            <em>react.js</em>
-          </strong>
-          :
-        </p>
-        <ul>{techsFormatted}</ul>
-        {result}
-        {personAge}
-      </div>
-    </div>
-  );
-
-  const copyRight = "Copyright 2020";
-
-  const footer = (
-    <div>
-      <div className="bg-blue-300">
-        <p>{copyRight}</p>
-      </div>
-    </div>
-  );
-
   return (
-    <div className="flex flex-col gap-4 divide-y-2 text-center">
-      {header}
-      {main}
-      {footer}
+    <div className="flex flex-col gap-2">
+      {tools}
+      {subscribe}
+      {userCard}
     </div>
   );
 }
